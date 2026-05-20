213a3de8754a84072ff2845fdb6b137b1cebb992
Murilo Santos <142051797+murilosantosb@users.noreply.github.com>
2026-05-20 11:34:49 -0300

    Començando a implementar o JS.

--- backend/src/main/java/br/com/notelab/backend/Config/SecurityConfig.java
+++ backend/src/main/java/br/com/notelab/backend/Config/SecurityConfig.java
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 package br.com.notelab.backend.Config;
-=======
 
->>>>>>> 0419367bcd67a3494fb9b0af8e94f6399c53297b
 import br.com.notelab.backend.Services.UserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,21 +27,27 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder builder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        builder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
-        return builder.build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .userDetailsService(userDetailsService)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/register", "/registrar", "/matter/**", "/caderno/**", "/note/**", "/img/**", "/**.css", "/*.css").permitAll()
+                        .requestMatchers(
+                                "/login",
+                                "/register",
+                                "/registrar",
+                                "/matter/**",
+                                "/caderno/**",
+                                "/img/**",
+                                "/**.css",
+                                "/*.css"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
